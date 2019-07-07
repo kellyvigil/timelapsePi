073a72832ca219d6fc313857ccacc60f648b4f84
Kelly Vigil <kellywvigil@gmail.com>
2019-07-07 13:39:27 -0400

    first commit

---
+++ test/test.sh
@@ -0,0 +1,6 @@
+raspistill -n  -w 1920 -h 1080 -q 100 -ISO 100 -ex beach -ag 1.0 -dg 1.0 -awb off -o beach.png -e png
+raspistill -n  -w 1920 -h 1080 -q 100 -ISO 100 -ex fireworks -ag 1.0 -dg 1.0 -awb off -o fireworks.png -e png
+raspistill -n  -w 1920 -h 1080 -q 100 -ISO 100 -ex night -ag 1.0 -dg 1.0 -awb off -o night.png -e png
+raspistill -n  -w 1920 -h 1080 -q 100 -ISO 100 -ex verylong -ag 1.0 -dg 1.0 -awb off -o verylong.png -e png
+raspistill -n  -w 1920 -h 1080 -q 100 -ISO 100 -ex auto -ag 1.0 -dg 1.0 -awb off -o auto.png -e png
+
